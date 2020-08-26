d4a08983d440dbda08b854099ad10e98a6408d13
gustavo2892 <gustavo2892@gmail.com>
2020-08-26 17:41:33 -0300

    Add AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Gustavo' }}>
+      <SignIn />
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
