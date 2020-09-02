5ec1f586b0116a71f03c058600b471f8badf24c1
gustavo2892 <gustavo2892@gmail.com>
2020-09-02 16:40:57 -0300

    Style Toast

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
+
     <GlobalStyle />
   </>
 );
